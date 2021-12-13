3c5c8690e6551c87711b77f98a0407da9fe5ff76
Cdric_art <cdric.art@gmail.com>
2021-12-13 14:32:34 +0100

    Initial commit

---
+++ boilerplate/boilerplateApp.swift
@@ -0,0 +1,18 @@
+//
+//  boilerplateApp.swift
+//  boilerplate
+//
+//  Created by Cédric Evrard on 13/12/2021.
+//
+//
+
+import SwiftUI
+
+@main
+struct boilerplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
